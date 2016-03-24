c2b5f9ee7f9ee4cef5d48bb08e907ade49a065be
BlueSkyBalmung <vincent.heit@gmail.com>
2016-03-24 16:11:42 +0100

    ajout des fonctions vide et tests vide

--- src/test/java/fr/ups/mdl/iaws/projectIAWS/ServiceTrajetTest.java
+++ src/test/java/fr/ups/mdl/iaws/projectIAWS/ServiceTrajetTest.java
@@ -4,5 +4,19 @@ package fr.ups.mdl.iaws.projectIAWS;
 import junit.framework.TestCase;
 
 public class ServiceTrajetTest extends TestCase{
-
+	public void testStationVide(){
+		
+	}
+	public void testStationnonComplete(){
+		
+	}
+	public void testRisqueChausseMouille(){
+		
+	}
+	public void testTempsTrajetPied(){
+		
+	}
+	public void testTempsTrajetVelo(){
+		
+	}
 }
