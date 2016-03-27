b8887d09a3e316b8aee5fb58d12d30aca4474fa0
BlueSkyBalmung <vincent.heit@gmail.com>
2016-03-27 14:06:45 +0200

    Détail sur ServiceVelib

---
+++ src/main/java/fr/ups/mdl/iaws/projectIAWS/ServiceVelib.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package fr.ups.mdl.iaws.projectIAWS;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonValue;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author 21002269
+ *
+ */
+public class ServiceVelib {
+	private String cleJCDecaux="039a8fcb1cfb47bcaa20e9ed00f0f07f64bff95e";
+	private static final String API_URI =  "https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=039a8fcb1cfb47bcaa20e9ed00f0f07f64bff95e";
+	public void stationNonVide(String adresse){
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(API_URI);
+		try {
+			JsonArray result = target.request(MediaType.APPLICATION_JSON).get(JsonArray.class);
+			JsonArray closer;
+			for(int i=0;i<result.size();i++){
+				JsonObject ite=result.getJsonObject(i);
+				ite.getString("lat");
+				ite.getString("lng");
+				
+			}
+		
+			//String pageName = obj.getJsonObject();
+		}catch (InternalServerErrorException e) {
+			// e.printStackTrace();
+			System.err.println("Réponse HTTP " + e.getResponse().getStatus());
+		}
+		
+
+	}
+	public void stationNonComplete(String adresse){
+		
+	}
+	
+	
+}
