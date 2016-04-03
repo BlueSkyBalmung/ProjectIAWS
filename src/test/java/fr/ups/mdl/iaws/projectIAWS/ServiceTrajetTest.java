4ac3d5c06b31d0967e3a7e70c67cbf7a7b0f3239
BlueSkyBalmung <vincent.heit@gmail.com>
2016-04-03 20:11:16 +0200

    mise à jour de l'arrayList dans les test Unit

--- src/test/java/fr/ups/mdl/iaws/projectIAWS/ServiceTrajetTest.java
+++ src/test/java/fr/ups/mdl/iaws/projectIAWS/ServiceTrajetTest.java
@@ -2,6 +2,7 @@ package fr.ups.mdl.iaws.projectIAWS;
 
 
 import java.awt.List;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -16,26 +17,26 @@ public class ServiceTrajetTest {
 	@Test
 	public void testStationVide(){
 		ServiceVelib serviceVelib=new ServiceVelib();
-		Station[] stationsNonVide=serviceVelib.stationNonVide("Université Paul Sabatier Toulouse");
-		assert  stationsNonVide.length==3:"nombre de stations respectés";
-		assert  stationsNonVide[0].getNom().isEmpty():"pas d'adresse vide dans 0";
-		assert  stationsNonVide[1].getNom().isEmpty():"pas d'adresse vide dans 1";
-		assert  stationsNonVide[2].getNom().isEmpty():"pas d'adresse vide dans 2";
-		assert stationsNonVide[0].getNombreVelos()>0:"station 0 non complète";
-		assert stationsNonVide[1].getNombreVelos()>0:"station 1 non complète";
-		assert stationsNonVide[2].getNombreVelos()>0:"station 2 non complète";
+		ArrayList<Station> stationsNonVide=serviceVelib.stationsNonVides("Université Paul Sabatier Toulouse");
+		assert  stationsNonVide.size()==3:"nombre de stations respectés";
+		assert  stationsNonVide.get(0).getAdresse().isEmpty():"pas d'adresse vide dans 0";
+		assert  stationsNonVide.get(1).getAdresse().isEmpty():"pas d'adresse vide dans 1";
+		assert  stationsNonVide.get(2).getAdresse().isEmpty():"pas d'adresse vide dans 2";
+		assert stationsNonVide.get(0).getNombreVelos()>0:"station 0 a des velos";
+		assert stationsNonVide.get(1).getNombreVelos()>0:"station 1 a des velos";
+		assert stationsNonVide.get(2).getNombreVelos()>0:"station 2 a des velos";
 	}
 	@Test
 	public void testStationnonComplete(){
 		ServiceVelib serviceVelib=new ServiceVelib();
-		Station[] stationsNonVide=serviceVelib.stationNonComplete("Université Paul Sabatier Toulouse");
-		assert  stationsNonVide.length==3:"nombre de stations respectés";
-		assert  stationsNonVide[0].getNom().isEmpty():"pas d'adresse vide dans 0";
-		assert  stationsNonVide[1].getNom().isEmpty():"pas d'adresse vide dans 1";
-		assert  stationsNonVide[2].getNom().isEmpty():"pas d'adresse vide dans 2";
-		assert stationsNonVide[0].getNombrePlaces()>0:"station 0 non complète";
-		assert stationsNonVide[1].getNombrePlaces()>0:"station 1 non complète";
-		assert stationsNonVide[2].getNombrePlaces()>0:"station 2 non complète";
+		ArrayList<Station> stationsNonVide=serviceVelib.stationsNonCompletes("Université Paul Sabatier Toulouse");
+		assert  stationsNonVide.size()==3:"nombre de stations respectés";
+		assert  stationsNonVide.get(0).getAdresse().isEmpty():"pas d'adresse vide dans 0";
+		assert  stationsNonVide.get(1).getAdresse().isEmpty():"pas d'adresse vide dans 1";
+		assert  stationsNonVide.get(2).getAdresse().isEmpty():"pas d'adresse vide dans 2";
+		assert stationsNonVide.get(0).getNombrePlaces()>0:"station 0 non complète";
+		assert stationsNonVide.get(1).getNombrePlaces()>0:"station 1 non complète";
+		assert stationsNonVide.get(2).getNombrePlaces()>0:"station 2 non complète";
 	}
 	@Test
 	public void testRisqueChausseMouille(){
